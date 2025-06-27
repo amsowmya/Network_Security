859588d58b301c98bcd5609f7e137c77fd877dbb
amsowmya <soumya.pdit@gmail.com>
2025-06-27 22:05:09 +0530

    added model trainer component

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -63,25 +63,37 @@ class TrainingPipeline:
             logging.info(f"Started Data Transformation")
             data_transformation = DataTransformation(data_validation_artifact=data_validation_artifact, 
                                                      data_transformation_config=self.data_transformation_config)
-            data_transformation_artifact = data_transformation.initiate_data_validation()
+            data_transformation_artifact = data_transformation.initiate_data_transformation()
             return data_transformation_artifact
         
         except Exception as e:
             raise NetworkSecurityException(e, sys)
         
-    def start_model_trainer(self):
+    def start_model_trainer(self, data_transformation_artifact: DataTransformationArtifact):
         try:
-            pass 
+            self.model_trainer_config = ModelTrainerConfig(training_pipeline_config=self.training_pipeline_config) 
+            logging.info(f"Started Model Trainer")
+            model_trainer = ModelTrainer(data_transformation_artifact=data_transformation_artifact,
+                                         model_trainer_config=self.model_trainer_config)
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
+        
         except Exception as e:
             raise NetworkSecurityException(e, sys)
         
-    def start_model_evaluation(self):
+    def start_model_evaluation(self, data_validation_artifact:DataValidationArtifact, model_trainer_artifact: ModelTrainerArtifact):
         try:
-            pass 
+            self.model_evaluation_config = ModelEvaluationConfig(training_pipeline_config=self.training_pipeline_config)
+            logging.info(f"Started Model Evaluation")
+            model_evaluation = ModelEvaluation(model_evaluation_config=self.model_evaluation_config,
+                                               data_validation_artifact=data_validation_artifact,
+                                               model_trainer_artifact=model_trainer_artifact)
+            model_evaluation_artifact = model_evaluation.initiate_model_evaluation()
+            return model_evaluation_artifact
         except Exception as e:
             raise NetworkSecurityException(e, sys)
         
-    def start_model_pusher(self):
+    def start_model_pusher(self, model_eval_artifact: ModelEvaluationArtifact):
         try:
             pass 
         except Exception as e:
@@ -91,6 +103,12 @@ class TrainingPipeline:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact)
-            self.start_data_transformation(data_validation_artifact)
+            data_transformation_artifact = self.start_data_transformation(data_validation_artifact)
+            model_trainer_artifact = self.start_model_trainer(data_transformation_artifact)
+            model_eval_artifact = self.start_model_evaluation(data_validation_artifact=data_validation_artifact, model_trainer_artifact=model_trainer_artifact)
+            if not model_eval_artifact.is_model_accepted:
+                raise Exception("Trained model is not better than the best model.")
+            
+            model_pusher_artifact = self.start_model_pusher(model_eval_artifact)
         except Exception as e:
             raise NetworkSecurityException(e, sys)
